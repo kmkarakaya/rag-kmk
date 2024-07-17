598f8811e258708f61807bfed5515ce0da87c6d0
kmkarakaya <kmkarakaya@gmail.com>
2024-07-17 23:18:30 +0300

    add info function

--- rag_kmk/common.py
+++ rag_kmk/common.py
@@ -5,3 +5,11 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+def info():
+    """Prints information about the module.
+    """
+    print("This is the common module.")
+    print("It contains common functions and classes used by the other modules.")
+
+    
