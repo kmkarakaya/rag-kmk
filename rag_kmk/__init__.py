4a6a5edad04182a7a9e2646ca8334a99bd199a5c
kmkarakaya <kmkarakaya@gmail.com>
2024-07-18 18:52:46 +0300

    __all__ added

--- rag_kmk/__init__.py
+++ rag_kmk/__init__.py
@@ -1,6 +1,10 @@
 """Top-level package for rag-kmk."""
+from .common import hello_world, info, get_version
+
+
+
 
 __author__ = """Murat Karakaya"""
 __email__ = "kmkarakaya@gmail.com"
-__version__ = "0.0.5"
+__version__ = "0.0.4"
 __all__ = ['hello_world', 'info', 'get_version']
