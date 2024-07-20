3d8d857ca2b1e658db7334cd1f5fb7b2ea9170c2
kmkarakaya <kmkarakaya@gmail.com>
2024-07-20 17:13:41 +0300

    logger implemented

--- rag_kmk/knowledge_base/document_loader.py
+++ rag_kmk/knowledge_base/document_loader.py
@@ -2,9 +2,9 @@ import os
 import fitz  # PyMuPDF
 from rag_kmk import CONFIG
 from rag_kmk.utils.logging_utils import setup_logger
-from docx import Document  # python-docx
 
-def load_documents(directory_path):
+
+def load_documents(document_directory_path):
     """
     Loads documents from a specified directory. Supported file types are defined in CONFIG['supported_file_types'].
     Currently supports .txt, .pdf, and .docx files.
@@ -15,16 +15,17 @@ def load_documents(directory_path):
     Returns:
     - list: A list of document contents as strings.
     """
-    # Initialize the logger
-    logger = setup_logger('document_loader', 'rag-kmk\\rag_kmk\\logs\\document_loader.log')
+
+    # Initialize the logger with relative path
+    logger = setup_logger('document_loader', 'logs/document_loader.log')
     documents = []
 
-    if not os.path.isdir(directory_path):
-        logger.error(f'{directory_path} is not a directory.')
+    if not os.path.isdir(document_directory_path):
+        logger.error(f'{document_directory_path} is not a directory.')
         return documents
 
-    for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)
+    for filename in os.listdir(document_directory_path):
+        file_path = os.path.join(document_directory_path, filename)
         file_extension = os.path.splitext(filename)[1]
 
         if file_extension in CONFIG['supported_file_types']:
@@ -32,15 +33,15 @@ def load_documents(directory_path):
                 if file_extension == '.txt':
                     with open(file_path, 'r') as file:
                         document = file.read()
-                        documents.append(document)
-                        logger.info(f'Text document loaded successfully from {file_path}')
+                    documents.append(document)
+                    logger.info(f'Text document loaded successfully from {file_path}')
                 elif file_extension == '.pdf':
                     with fitz.open(file_path) as doc:
                         text = ''
                         for page in doc:
                             text += page.get_text()
-                        documents.append(text)
-                        logger.info(f'PDF document loaded successfully from {file_path}')
+                    documents.append(text)
+                    logger.info(f'PDF document loaded successfully from {file_path}')
                 elif file_extension == '.docx':
                     doc = Document(file_path)
                     text = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
