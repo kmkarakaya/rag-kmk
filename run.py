485c511f3fce2724e15e0da93411cb80e8bdca2a
kmkarakaya <kmkarakaya@gmail.com>
2025-01-09 23:28:16 +0300

    refactor: Update CONFIG dictionary and print LLM values.

--- run.py
+++ run.py
@@ -7,10 +7,12 @@ from rag_kmk.chat_flow import generateAnswer, generate_LLM_answer, RAG_LLM, run_
 
 
 def main():
-    # write the necessary code to print the CONFIG dictionary AI!
+
+    
+    #print the llm values in CONFIG dictionary AI!
     CONFIG.update({'LLM': {'settings': {'system_prompt': 'Q: What is'}}})
     # # Load the documents
-    # knowledge_base= build_knowledge_base(r'.\tests\sample_documents')
+    # knowledge_base= build_knowledge_base(r'.\tests\sample_documents') 
     # print("-----------------"*4)
     # print(CONFIG)    
 
