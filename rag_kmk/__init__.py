6e0c01f7efef2f1a280fc58e8e90571594627e17
kmkarakaya <kmkarakaya@gmail.com>
2025-10-13 13:52:43 +0300

    Bump version: 0.0.49 → 0.0.50

--- rag_kmk/__init__.py
+++ rag_kmk/__init__.py
@@ -7,7 +7,7 @@ explicitly at runtime.
 
 __author__ = "Murat Karakaya"
 __email__ = "kmkarakaya@gmail.com"
-__version__ = "0.0.49"
+__version__ = "0.0.50"
 
 from .config.config import load_config, mask_config
 
